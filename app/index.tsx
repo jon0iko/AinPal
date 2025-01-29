32a3716d345b220acbbb64aac513191ecedba232
jon0iko <price6967@gmail.com>
2025-01-30 02:28:49 +0600

    Revert "fixing splash"
    This reverts commit 17cc77b623d4f8784c465a162b99ba92fa7944a8.

--- app/index.tsx
+++ app/index.tsx
@@ -1,48 +1,21 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { Redirect, useRouter } from "expo-router";
+import React, { useEffect } from "react";
+import { Redirect, useRouter } from "expo-router"; 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import "react-native-gesture-handler";
 import { useAuth } from "@clerk/clerk-expo";
-import * as SplashScreen from "expo-splash-screen";
-import { View, Text } from "react-native";
-
-SplashScreen.preventAutoHideAsync();
-
-// Set the animation options. This is optional.
-// SplashScreen.setOptions({
-//   duration: 1000,
-//   fade: true,
-// });
 
 export default function Index() {
   const { isSignedIn } = useAuth();
-  const router = useRouter();
-  const [appIsReady, setAppIsReady] = useState(false);
-
-    useEffect(() => {
-      async function prepare() {
-        try {
-       
-          await new Promise((resolve) => setTimeout(resolve, 2000));
-        } catch (e) {
-          console.warn(e);
-        } finally {
- 
-          setAppIsReady(true);
-        //  SplashScreen.hide();
-        await SplashScreen.hideAsync();
-        }
-      }
-
-      prepare();
-    }, []);
-
+  const router = useRouter(); 
+  if (isSignedIn) {
+    return <Redirect href={"/(root)/(tabs)/home"} />;
+  }
 
   useEffect(() => {
     const resetOnboarding = async () => {
       try {
         await AsyncStorage.removeItem("onboarded");
-        console.log("Onboarding reset");
+        console.log("Onboarding reset"); 
       } catch (error) {
         console.error("Failed to reset onboarding status:", error);
       }
@@ -50,18 +23,19 @@ export default function Index() {
     resetOnboarding();
   }, []);
 
+
   useEffect(() => {
     const checkOnboardingStatus = async () => {
       try {
         const onboarded = await AsyncStorage.getItem("onboarded");
-        console.log("Onboarding Status:", onboarded);
+        console.log("Onboarding Status:", onboarded); 
 
         if (onboarded === "true") {
           console.log("Navigating to home...");
           router.replace("/(root)/(tabs)/home");
         } else {
           console.log("Redirecting to Onboarding...");
-          router.replace("/onboarding");
+          router.replace("/onboarding"); 
         }
       } catch (error) {
         console.error("Failed to check onboarding status:", error);
@@ -70,17 +44,5 @@ export default function Index() {
     checkOnboardingStatus();
   }, [router]);
 
-   if (!appIsReady) {
-     return null;
-   }
-
-   if (isSignedIn) {
-     return <Redirect href={"/(root)/(tabs)/home"} />;
-   }
-
-  return (
-    <View>
-      <Text>App is ready.</Text>
-    </View>
-  );
+  return null; 
 }
