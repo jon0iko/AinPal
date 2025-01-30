bf2b1fe45fc4a1588ba41880dbc27f850b25b0e2
jon0iko <price6967@gmail.com>
2025-01-30 22:00:58 +0600

    Revert "fixing code for splash"
    This reverts commit 7985672f251f49bae38e225a6a96a323948aece5.

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,21 @@
 import React, { useEffect } from "react";
-import { Redirect, useRouter } from "expo-router";
+import { Redirect, useRouter } from "expo-router"; 
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import * as SplashScreen from "expo-splash-screen";
-
-// Keep the splash screen visible while we fetch resources
-SplashScreen.preventAutoHideAsync();
+//import "react-native-gesture-handler";
+// import { useAuth } from "@clerk/clerk-expo";
 
 export default function Index() {
-  const router = useRouter();
+  // const { isSignedIn } = useAuth();
+  const router = useRouter(); 
+  // if (isSignedIn) {
+  //   return <Redirect href={"/(root)/(tabs)/home"} />;
+  // }
 
   useEffect(() => {
     const resetOnboarding = async () => {
       try {
         await AsyncStorage.removeItem("onboarded");
-        console.log("Onboarding reset");
+        console.log("Onboarding reset"); 
       } catch (error) {
         console.error("Failed to reset onboarding status:", error);
       }
@@ -21,28 +23,26 @@ export default function Index() {
     resetOnboarding();
   }, []);
 
+
   useEffect(() => {
     const checkOnboardingStatus = async () => {
       try {
         const onboarded = await AsyncStorage.getItem("onboarded");
-        console.log("Onboarding Status:", onboarded);
+        console.log("Onboarding Status:", onboarded); 
 
         if (onboarded === "true") {
           console.log("Navigating to home...");
           router.replace("/(root)/(tabs)/home");
         } else {
           console.log("Redirecting to Onboarding...");
-          router.replace("/onboarding");
+          router.replace("/onboarding"); 
         }
-        // Hide the splash screen once we're done
-        await SplashScreen.hideAsync();
       } catch (error) {
         console.error("Failed to check onboarding status:", error);
-        await SplashScreen.hideAsync();
       }
     };
     checkOnboardingStatus();
   }, [router]);
 
-  return null;
+  return null; 
 }
